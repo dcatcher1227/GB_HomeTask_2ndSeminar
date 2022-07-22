10fd5ba7a14840f44a243b96786927f7411763ae
DC <d.catcher2014@yandex.ru>
2022-07-22 16:02:56 +0300

    создал программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+if (num <= 99)
+{
+    Console.WriteLine("Цифры нет ");
+}
+string s1 = num.ToString();
+char ch3 = s1[2];
+Console.WriteLine(s1[2]);

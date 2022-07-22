987e187a64861b17c7ea69fd1811d35a60441907
DC <d.catcher2014@yandex.ru>
2022-07-22 11:28:04 +0300

    Написал программу которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите 3х значное число");
+int number = int.Parse (Console.ReadLine( ));
+int sacondDigit = (number / 10) % 10;
+Console.WriteLine(sacondDigit);
